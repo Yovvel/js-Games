099bff388f1d76f4dc596a7096cf077e41ec2ec7
Joffrey <joffrey.nolting@gmail.com>
2021-10-28 15:09:32 +0200

    created the board  in js

--- memory-game/app.js
+++ memory-game/app.js
@@ -51,4 +51,31 @@ document.addEventListener('DOMContentLoaded', () => {
       img: 'images/pizza.png'
     },
   ]
+
+  const grid = document.querySelector('.grid')
+  var cardsChosen  =  []
+  var cardsChosenId = []
+
+  // create the board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++){
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+  // check for matches
+
+
+  // flip the card
+  function flipCard() {
+    var cardId  = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+  }
+
+
+  createBoard()
 })
